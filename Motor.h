2073b1f04d89d253ad53384362c71df5f2ce53c1
Shivangi <shivangi.deo18@vit.edu>
2020-02-02 17:46:46 +0530

    Methods for ClassMotor created

--- Motor.h
+++ Motor.h
@@ -8,12 +8,33 @@
 class Motor
 {
     private:
-    int _Pin_No;
+    int _DirPin;
     bool _Dir;
+    int _PWM_Pin
     int _PWM;
 
     public:
-    Motor(int _Pin_No, bool _Dir);
-    void Toggle_Diretion();
-    void ChangeSpeed(int _PWM);
+    Motor(int _DirPin, bool _Dir, int _PWM_Pin);
+    void SetDirection();
+    void ToggleDirection();
+    void SetSpeed(int _PWM);
+
 };
+
+void Motor::ToggleDirection()
+{
+    _Dir = !(_Dir);
+    digitalWrite(_DirPin,_Dir);
+
+}
+
+void Motor::SetDirection()
+{
+    digitalWrite(_DirPin,_Dir);
+}
+
+void Motor::SetSpeed(int _PWM)
+{
+    analogWrite(_PWM_Pin,_PWM);
+    
+}
