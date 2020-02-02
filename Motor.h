ef7e2b58eb24eff8939770aee5bdd3bfc2a580aa
Shivangi <shivangi.deo18@vit.edu>
2020-02-02 19:55:51 +0530

    Methods for ClassMotor tested.

--- Motor.h
+++ Motor.h
@@ -4,18 +4,23 @@
  * Name of Member Variables :
  *
  */
+#ifndef _MOTOR_H_
+#define _MOTOR_H_
 
 class Motor
 {
     private:
     int _DirPin;
     bool _Dir;
-    int _PWM_Pin
+    int _PWM_Pin;
     int _PWM;
 
     public:
-    Motor(int _DirPin, bool _Dir, int _PWM_Pin)
+     Motor(int DirPin, bool Dir, int PWM_Pin)
     {
+        _DirPin = DirPin;
+        _Dir = Dir;
+        _PWM_Pin = PWM_Pin;
         pinMode(_DirPin,OUTPUT);
         pinMode(_PWM_Pin,OUTPUT);
     }
@@ -41,5 +46,10 @@ void Motor::SetDirection()
 void Motor::SetSpeed(int _PWM)
 {
     analogWrite(_PWM_Pin,_PWM);
-    
+    Serial.print("PIN :");
+    Serial.print(_PWM_Pin);
+    Serial.print("PWM :");
+    Serial.println(_PWM);
 }
+
+#endif
