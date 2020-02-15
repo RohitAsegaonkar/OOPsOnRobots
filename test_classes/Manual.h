a45e798faf639530ec7acc196f811a734267a79c
Amogh_Amonkar <amogh.amonkar@gmail.com>
2020-02-15 20:39:58 +0530

    Made objects of all the classes private and changed their names.

--- test_classes/Manual.h
+++ test_classes/Manual.h
@@ -9,31 +9,43 @@
 class Manual
 {
   private:
-                                       //Variable to store the resolved value of yaw from -180 to 180
-    float Shifted_Yaw = 0;
+    float Shifted_Yaw = 0;//Variable to store the resolved value of yaw from -180 to 180
+    Encoder _X;
+    Encoder _Y;
+    Mpu _V;
+    Motor _M1, _M2, _M3;
+
   public:
 
-    Manual(/* args */);
+    Manual();
+
+    Manual(Motor m1, Motor m2, Motor m3, Mpu v, Encoder x, Encoder y)
+    {
+      //Assigning the Motor Object
+      _M1 = m1;
+      _M2 = m2;
+      _M3 = m3;
+      //Assigning the Encoder Object
+      _X = x;
+      _Y = y;
+      //Assigning the MPU object
+      _V = v;
+    }
+
+
     //Boolean Variables to set direction of Wheels
     bool dirW1 = 0 ;
     bool dirW2 ;
     bool dirW3 ;
     float Yaw = 0;   
+
     /************ PWM Values ************/
-  #define Maxpwm 200.00
-  #define basePwm 150
+    #define Maxpwm 200.00
+    #define basePwm 150
     int pwmm1, pwmm2, pwmm3;
 
     /************************ Objects Created *********************************/
-    Encoder X;
-    Encoder Y;
-    Mpu V;
 
-    //Motor M1;
-    // Motor M1(34, dirW1, 9);
-    //Motor M2(28, dirW2, 6);
-    //Motor M3(30, dirW3, 7);
-    Motor M1, M2, M3;
 
     void forwardManY(float kp_strm2_forward, float kp_strm3_forward, float kp_encoder_forward);
     void backwardManY(float KP_M2_Backward, float KP_M3_Backward, float KP_Enc_Backward);
@@ -44,7 +56,7 @@ class Manual
     void UpdateShiftedYaw(float Yaw_ref);
     void reset();
 
-    float error_forward;                                 //Variable to store the value of the X encoder as error.
+    float error_forward;                                 //Variable to store the value of the_X-Enc encoder as error.
     const float Kp_encoder_forward = 0.0 ;                       //Proportionality constant for the lateral error
     const float Kp_strm2_forward = 0.48 ;                       //Proportionality constant for the angular error for motor 2
     const float  Kp_strm3_forward = 0.48 ;                        //Proportionality constant for the angular error for motor 3
@@ -62,9 +74,10 @@ class Manual
     float error_encoder_back;
     float pwm_encoder_back;
     float error_sum_back;
+    /****************************************/
 
     /***** leftManX() Function Variables *****/
-    float error_left;                                     //Variable to store the value of the X encoder as error.
+    float error_left;                                     //Variable to store the value of the_X-Enc encoder as error.
     const float Kp_encoder_left = 0.02;
     const float Kp_strm1_left = 0.4;
     const float Kp_strm2_left = 0.6;
@@ -73,6 +86,7 @@ class Manual
     float error_encoder_left;
     float pwm_encoder_left;
     float error_sum_left;
+    /****************************************/
 
     /***** rightManX() Function Variables *****/
     float error_right;
@@ -84,9 +98,11 @@ class Manual
     float error_encoder_right;
     float pwm_encoder_right;
     float error_sum_right;
+    /****************************************/
 
     int final_ang = 0, correction_angle = 0;
     int count = 0;
+
     //PWM Given to motors for rotating
     float pwmm_ori;
 
@@ -117,14 +133,14 @@ Manual::Manual()
 
 void Manual::reset()
 {
-  Y.encodervalue = 0;
-  X.encodervalue = 0;
+  _Y.encodervalue = 0;
+  _X.encodervalue = 0;
   //Last_Yaw = V.readMpu(2);
   //A.Shifted_Yaw = A.Yaw;
   Serial.print("In Reset");
   Serial.print("\tA.Yaw = ");  
-  Serial.println(V.readMpu(2));
-  UpdateShiftedYaw(V.readMpu(2));
+  Serial.println(_V.readMpu(2));
+  UpdateShiftedYaw(_V.readMpu(2));
   pwmm1 = 0;
   pwmm2 = 0;
   pwmm3 = 0;
@@ -146,15 +162,11 @@ void Manual::UpdateShiftedYaw(float Yaw_ref)
 */
 void Manual :: forwardManY(float kp_strm2_forward, float kp_strm3_forward, float kp_encoder_forward)
 {
-  Motor M1(26, 1, 6);
-  Motor M2(38, 1, 12);
-  Motor M3(36, 1, 11);
-
   Serial.println("In F");
-  Yaw = V.readMpu(2);                          // Reading Mpu Values
+  Yaw = _V.readMpu(2);                          // Reading Mpu Values
   error_forward = Yaw - Shifted_Yaw;
 
-  error_encoder_forward = X.encodervalue;
+  error_encoder_forward = _X.encodervalue;
   pwm_encoder_forward = kp_encoder_forward * (error_encoder_forward);
 
   pwmm2 = basePwm + kp_strm2_forward * (error_forward) - pwm_encoder_forward ;
@@ -171,13 +183,13 @@ void Manual :: forwardManY(float kp_strm2_forward, float kp_strm3_forward, float
   if (pwmm3 > Maxpwm)
     pwmm3 = Maxpwm;
 
-  M1.SetDirection();
-  M2.SetDirection();
-  M3.SetDirection();
+  _M1.SetDirection();
+  _M2.SetDirection();
+  _M3.SetDirection();
 
-  M1.SetSpeed(0);
-  M2.SetSpeed(abs(pwmm2));
-  M3.SetSpeed(abs(pwmm3));
+  _M1.SetSpeed(0);
+  _M2.SetSpeed(abs(pwmm2));
+  _M3.SetSpeed(abs(pwmm3));
 
   /********************************************* SERIAL PRINTING DATA ***************************************************/
 
@@ -191,7 +203,7 @@ void Manual :: forwardManY(float kp_strm2_forward, float kp_strm3_forward, float
   Serial.print("\tError encoder: ");
   Serial.print(error_encoder_forward);
   Serial.print("\tencodervalue1 :      ");
-  Serial.print(Y.encodervalue);
+  Serial.print(_Y.encodervalue);
   Serial.print("\tkp_strm2_forward :      ");
   Serial.print(kp_strm2_forward);
   Serial.print("\tPWM:  ");
@@ -215,15 +227,10 @@ void Manual :: forwardManY(float kp_strm2_forward, float kp_strm3_forward, float
 */
 void Manual :: backwardManY(float KP_M2_Backward, float KP_M3_Backward, float KP_Enc_Backward)
 {
-
-  Motor M1(26, 0, 6);
-  Motor M2(38, 0, 12);
-  Motor M3(36, 0, 11);
-
-  Yaw = V.readMpu(2);                         // Reading Mpu Values
+  Yaw = _V.readMpu(2);                         // Reading Mpu Values
   error_back = Yaw - Shifted_Yaw;
 
-  error_encoder_back = X.encodervalue;
+  error_encoder_back = _X.encodervalue;
   pwm_encoder_back = KP_Enc_Backward * (error_encoder_back);
 
   pwmm2 = basePwm - KP_M2_Backward * (error_back) - pwm_encoder_back;
@@ -235,13 +242,13 @@ void Manual :: backwardManY(float KP_M2_Backward, float KP_M3_Backward, float KP
   if (pwmm3 > Maxpwm)
     pwmm3 = Maxpwm;
 
-  M1.SetDirection();
-  M2.SetDirection();
-  M3.SetDirection();
+  _M1.SetDirection();
+  _M2.SetDirection();
+  _M3.SetDirection();
 
-  M1.SetSpeed(0);
-  M2.SetSpeed(abs(pwmm2));
-  M3.SetSpeed(abs(pwmm3));
+  _M1.SetSpeed(0);
+  _M2.SetSpeed(abs(pwmm2));
+  _M3.SetSpeed(abs(pwmm3));
 
   /********************************************* SERIAL PRINTING DATA ***************************************************/
 
@@ -255,7 +262,7 @@ void Manual :: backwardManY(float KP_M2_Backward, float KP_M3_Backward, float KP
   Serial.print("\tError encoder: ");
   Serial.print(error_encoder_forward);
   Serial.print("\tencodervalue1 :      ");
-  Serial.print(Y.encodervalue);
+  Serial.print(_Y.encodervalue);
   // Serial.print("\tkp_strm2_forward :      ");
   // Serial.print(kp_strm2_forward);
   Serial.print("\tPWM:  ");
@@ -278,14 +285,10 @@ void Manual :: backwardManY(float KP_M2_Backward, float KP_M3_Backward, float KP
 */
 void Manual :: leftManX(float KP_M1_Left, float KP_M2_Left, float KP_M3_Left, float KP_Enc_Left)
 {
-  Motor M1(26, 1, 6);
-  Motor M2(38, 1, 12);
-  Motor M3(36, 0, 11);
-
-  Yaw = V.readMpu(2);
+  Yaw = _V.readMpu(2);
   error_left = Yaw - Shifted_Yaw;                                                 //Calculate the angular shift of the bot. Yaw_ref is the reference yaw value from the previous function                       //Calculating the basepwm in proportion with the error
 
-  error_encoder_left = Y.encodervalue;                                            //Error for locomotion in X direction is given by the y encoder
+  error_encoder_left = _Y.encodervalue;                                            //Error for locomotion in X direction is given by the y encoder
   pwm_encoder_left = KP_Enc_Left * (error_encoder_left);                          //Calculating the pwm error
 
   pwmm1 = basePwm - KP_M1_Left * (error_left) - pwm_encoder_left;                 //Calculating the pwm for motor 1 according to the equations of velocities
@@ -301,14 +304,14 @@ void Manual :: leftManX(float KP_M1_Left, float KP_M2_Left, float KP_M3_Left, fl
   if (pwmm3 > Maxpwm)
     pwmm3 = Maxpwm / 2;
 
-  M1.SetDirection();
-  M2.SetDirection();
-  M3.SetDirection();
+  _M1.SetDirection();
+  _M2.SetDirection();
+  _M3.SetDirection();
 
 
-  M1.SetSpeed(abs(pwmm1));
-  M2.SetSpeed(abs(pwmm2));
-  M3.SetSpeed(abs(pwmm3));
+  _M1.SetSpeed(abs(pwmm1));
+  _M2.SetSpeed(abs(pwmm2));
+  _M3.SetSpeed(abs(pwmm3));
 
   /********************************************* SERIAL PRINTING DATA ***************************************************/
 
@@ -321,7 +324,7 @@ void Manual :: leftManX(float KP_M1_Left, float KP_M2_Left, float KP_M3_Left, fl
   Serial.print("\tError encoder: ");
   Serial.print(error_encoder_left);
   Serial.print("\tencodervalue1 :      ");
-  Serial.print(X.encodervalue);
+  Serial.print(_X.encodervalue);
   // Serial.print("\tkp_strm2_forward :      ");
   // Serial.print(kp_strm2_forward);
   Serial.print("\tPWM:  ");
@@ -344,12 +347,9 @@ void Manual :: leftManX(float KP_M1_Left, float KP_M2_Left, float KP_M3_Left, fl
 */
 void Manual :: rightManX(float KP_M1_Right, float KP_M2_Right, float KP_M3_Right, float KP_Enc_Right)
 {
-  Motor M1(26, 0, 6);
-  Motor M2(38, 0, 12);
-  Motor M3(36, 1, 11);
-  Yaw = V.readMpu(2);
+  Yaw = _V.readMpu(2);
 
-  error_encoder_right = Y.encodervalue;
+  error_encoder_right = _Y.encodervalue;
   pwm_encoder_right = KP_Enc_Right * (error_encoder_right);
 
   pwmm1 = basePwm + KP_M1_Right * (error_right) - pwm_encoder_right;
@@ -365,13 +365,13 @@ void Manual :: rightManX(float KP_M1_Right, float KP_M2_Right, float KP_M3_Right
   if (pwmm3 > Maxpwm)
     pwmm3 = Maxpwm / 2;
 
-  M1.SetDirection();
-  M2.SetDirection();
-  M3.SetDirection();
+  _M1.SetDirection();
+  _M2.SetDirection();
+  _M3.SetDirection();
 
-  M1.SetSpeed(abs(pwmm1));
-  M2.SetSpeed(abs(pwmm2));
-  M3.SetSpeed(abs(pwmm3));
+  _M1.SetSpeed(abs(pwmm1));
+  _M2.SetSpeed(abs(pwmm2));
+  _M3.SetSpeed(abs(pwmm3));
 
   /********************************************* SERIAL PRINTING DATA ***************************************************/
   Serial.print("\tYaw: ");
@@ -383,7 +383,7 @@ void Manual :: rightManX(float KP_M1_Right, float KP_M2_Right, float KP_M3_Right
   Serial.print("\tError encoder: ");
   Serial.print(error_encoder_right);
   Serial.print("\tencodervalue1 :      ");
-  Serial.print(X.encodervalue);
+  Serial.print(_X.encodervalue);
   // Serial.print("\tkp_strm2_forward :      ");
   // Serial.print(kp_strm2_right);
   Serial.print("\tPWM:  ");
@@ -419,9 +419,6 @@ void Manual :: TTP_Man(int dir)
   Serial.print("\tShifted Yaw: ");
   Serial.println(Shifted_Yaw);
 
-  Motor M1(26, dirW1, 6);
-  Motor M2(38, dirW2, 12);
-  Motor M3(36, dirW3, 11);
   pwmm_ori = 50;
 
 
@@ -445,14 +442,14 @@ void Manual :: TTP_Man(int dir)
   pwmm_ori2 = pwmm_ori;
   pwmm_ori3 = pwmm_ori;
 
-  M1.SetDirection();
-  M2.SetDirection();
-  M3.SetDirection();
+  _M1.SetDirection();
+  _M2.SetDirection();
+  _M3.SetDirection();
 
 
-  M1.SetSpeed(pwmm_ori1);
-  M2.SetSpeed(pwmm_ori2);
-  M3.SetSpeed(pwmm_ori3);
+  _M1.SetSpeed(pwmm_ori1);
+  _M2.SetSpeed(pwmm_ori2);
+  _M3.SetSpeed(pwmm_ori3);
 }
 
 /*  Function Name       : M_Turn()
@@ -467,7 +464,7 @@ void Manual :: TurnMan(float KP_Orient, float KI_Angle, float req_angle, int dir
   while (rate_change != 0 || abs(error_ang) > 2)
   {
     // read from port 1, send to port 0:
-    Yaw = V.readMpu(2);
+    Yaw = _V.readMpu(2);
 
     final_ang  = pow(-1, !dir) * req_angle + Shifted_Yaw ;
     final_ang += ((final_ang < - 180) - (final_ang > 180)) * 360 ;
@@ -491,10 +488,12 @@ void Manual :: TurnMan(float KP_Orient, float KI_Angle, float req_angle, int dir
     }
 
     error_sum_ori = error_sum_ori + error_ang;
+
     if ((rate_change) > 0 && rate_change != error_ang)
     {
       count++;
     }
+
     if (count > 0)
     {
       if ((rate_change) > 0)
@@ -503,13 +502,8 @@ void Manual :: TurnMan(float KP_Orient, float KI_Angle, float req_angle, int dir
         dirW2 = !(dirW2);
         dirW3 = !(dirW3);
       }
-
     }
-    
-    Motor M1(26, dirW1, 6);
-    Motor M2(38, dirW2, 12);
-    Motor M3(36, dirW3, 11);
-    
+
     pwmm_ori = abs(error_ang) * KP_Orient;
 
     if (abs(error_ang) < 20)
@@ -520,13 +514,12 @@ void Manual :: TurnMan(float KP_Orient, float KI_Angle, float req_angle, int dir
     if (pwmm_ori > (Maxpwm / 3))
       pwmm_ori = (Maxpwm / 3);
 
-
     /********************************************* SERIAL PRINTING DATA ***************************************************/
 
     //    Serial.print("\tYaw: ");
     //    Serial.print(Yaw);
-            Serial.print("\tShifted Yaw: ");
-            Serial.println(Shifted_Yaw);
+          Serial.print("\tShifted Yaw: ");
+          Serial.println(Shifted_Yaw);
     //    Serial.print("\tError: ");
     //    Serial.print(error_ang);
     //    Serial.print("\tFinal: ");
@@ -547,19 +540,20 @@ void Manual :: TurnMan(float KP_Orient, float KI_Angle, float req_angle, int dir
     pwmm_ori2 = pwmm_ori;
     pwmm_ori3 = pwmm_ori;
 
-    M1.SetDirection();
-    M2.SetDirection();
-    M3.SetDirection();
+    _M1.SetDirection();
+    _M2.SetDirection();
+    _M3.SetDirection();
 
-    M1.SetSpeed(pwmm_ori1);
-    M2.SetSpeed(pwmm_ori2);
-    M3.SetSpeed(pwmm_ori3);
+    _M1.SetSpeed(pwmm_ori1);
+    _M2.SetSpeed(pwmm_ori2);
+    _M3.SetSpeed(pwmm_ori3);
 
     prev_error = error_ang;
   }
-  M1.SetSpeed(0);
-  M2.SetSpeed(0);
-  M3.SetSpeed(0);
+  _M1.SetSpeed(0);
+  _M2.SetSpeed(0);
+  _M3.SetSpeed(0);
+
   count = 0;
 }
 
