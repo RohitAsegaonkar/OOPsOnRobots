c3b401a7d1fd24a6cc671d61ec5e8876b00e547a
Neeraj <neeraj.sahasrabudhe18@vit.edu>
2020-02-20 19:19:23 +0530

    Forward and Backward tuning done

--- test_classes/test_classes.ino
+++ test_classes/test_classes.ino
@@ -19,7 +19,7 @@ Motor M3(36, 1, 11);
 
 Manual A(M1, M2, M3, mpu, X, Y);
 Autonomous Auto(M1, M2, M3, mpu, X, Y);
-  
+
 Piston Gripper(41, 39), Throwing(37, 35);
 
 /****************************************** PID Constants for forward function ******************************************/
@@ -78,36 +78,36 @@ void setup()
   Serial3.begin(115200);
 
 
-  
-/*
-  pinMode(42,OUTPUT);
-  pinMode(44,OUTPUT);
-  pinMode(46,OUTPUT);
-  pinMode(48,OUTPUT);
-*/
-  attachInterrupt(0,UpdateXEncoder,RISING); 
-  attachInterrupt(1,UpdateYEncoder,RISING);
+
+  /*
+    pinMode(42,OUTPUT);
+    pinMode(44,OUTPUT);
+    pinMode(46,OUTPUT);
+    pinMode(48,OUTPUT);
+  */
+  attachInterrupt(0, UpdateXEncoder, RISING);
+  attachInterrupt(1, UpdateYEncoder, RISING);
 }
 
 void loop()
 {
-  #if autonomous
+#if autonomous
   //Serial.println(X.encodervalue);
-  Auto.forwardAutoY(200, 0.48, 0.48, ((Maxpwm/200) - 0.1), 0.04, 0.04,0.00, 40, 50 );
-  Auto.backwardAutoY(200, 0, 0, (MaxPwm/200), 0, 0);
-  delay(2000);
-  #endif
-    
-  #if manual 
+  //Auto.forwardAutoY(200, 0.48, 0.48, ((Maxpwm/200)), 0.33, 0.33,0.00, 50, 40);
+  Auto.backwardAutoY(200, 0.25, 0.25, (Maxpwm / 200), 0.0625, 0.0000);
+  delay(20000);
+#endif
+
+#if manual
   if (Serial3.available())
   {
     command = Serial3.read();
   }
   //Serial.print(command);
 
-  if(command != 'S' && command != 's')
+  if (command != 'S' && command != 's')
   {
-    //Serial.println("In Not S condition");   
+    //Serial.println("In Not S condition");
     Non_S_Char = command;
   }
 
@@ -120,117 +120,117 @@ void loop()
   switch (command)
   {
     case 'F':
-        A.forwardManY(Kp_strm2_forward, Kp_strm3_forward, Kp_encoder_forward);
-        //Serial.println("In F");
+      A.forwardManY(Kp_strm2_forward, Kp_strm3_forward, Kp_encoder_forward);
+      //Serial.println("In F");
       break;
 
     case 'B':
-        A.backwardManY(Kp_strm2_back, Kp_strm3_back, Kp_encoder_back);
-        //Serial.println("In B");
-      break;   
+      A.backwardManY(Kp_strm2_back, Kp_strm3_back, Kp_encoder_back);
+      //Serial.println("In B");
+      break;
 
     case 'L':
-        A.leftManX(Kp_strm1_left,Kp_strm2_left, Kp_strm3_left, Kp_encoder_left);
-        //Serial.println("In L");
+      A.leftManX(Kp_strm1_left, Kp_strm2_left, Kp_strm3_left, Kp_encoder_left);
+      //Serial.println("In L");
       break;
 
     case 'R':
-        A.rightManX(Kp_strm1_left,Kp_strm2_left, Kp_strm3_left, Kp_encoder_left);
-        //Serial.println("In R");
+      A.rightManX(Kp_strm1_left, Kp_strm2_left, Kp_strm3_left, Kp_encoder_left);
+      //Serial.println("In R");
       break;
 
     case 'S':
-        M1.SetSpeed(0);
-        M2.SetSpeed(0);
-        M3.SetSpeed(0);
-        digitalWrite(37,0);    
-        digitalWrite(39,0);
-        digitalWrite(35,0);
-        digitalWrite(41,0);
-        //Serial.println("In s");
+      M1.SetSpeed(0);
+      M2.SetSpeed(0);
+      M3.SetSpeed(0);
+      digitalWrite(37, 0);
+      digitalWrite(39, 0);
+      digitalWrite(35, 0);
+      digitalWrite(41, 0);
+      //Serial.println("In s");
       break;
 
     case 's':
-        M1.SetSpeed(0);
-        M2.SetSpeed(0);
-        M3.SetSpeed(0);
-        digitalWrite(37,0);
-        digitalWrite(39,0);
-        digitalWrite(35,0);
-        digitalWrite(41,0);
-        Serial.println("In s");
+      M1.SetSpeed(0);
+      M2.SetSpeed(0);
+      M3.SetSpeed(0);
+      digitalWrite(37, 0);
+      digitalWrite(39, 0);
+      digitalWrite(35, 0);
+      digitalWrite(41, 0);
+      Serial.println("In s");
       break;
 
-      case 'g':
-        A.TurnMan(kp_ori, ki_ang, 15.00, 0);
-        A.reset();
+    case 'g':
+      A.TurnMan(kp_ori, ki_ang, 15.00, 0);
+      A.reset();
       break;
 
     case 'p':
-        A.TurnMan(kp_ori, ki_ang, 15.00, 1);
-        A.reset();
+      A.TurnMan(kp_ori, ki_ang, 15.00, 1);
+      A.reset();
       break;
 
     case 'c':
-        A.TTP_Man(0);
+      A.TTP_Man(0);
       break;
 
     case 'd':
-        A.TTP_Man(1);
+      A.TTP_Man(1);
       break;
 
     case 'y':
-      if (prevCommand =='S') 
+      if (prevCommand == 'S')
       {
         Piston_Press_Event++;
       }
-        Piston_Press_Event %= 3;
-        if(Piston_Press_Event == 0)
-        {
-          Gripper.Retract(); 
-          Serial.print("Piston_Press_Event:");
-          Serial.println(Piston_Press_Event);
-        }
-        if(Piston_Press_Event == 1)
-        {
-          Throwing.Extend();
-          Serial.print("Piston_Press_Event:");
-          Serial.println(Piston_Press_Event);
-        }
-        if(Piston_Press_Event == 2)
-        {
-          Throwing.Retract();
-          Gripper.Extend();
-          Serial.print("Piston_Press_Event:");
-          Serial.println(Piston_Press_Event);
-        }
-    break;
-    
+      Piston_Press_Event %= 3;
+      if (Piston_Press_Event == 0)
+      {
+        Gripper.Retract();
+        Serial.print("Piston_Press_Event:");
+        Serial.println(Piston_Press_Event);
+      }
+      if (Piston_Press_Event == 1)
+      {
+        Throwing.Extend();
+        Serial.print("Piston_Press_Event:");
+        Serial.println(Piston_Press_Event);
+      }
+      if (Piston_Press_Event == 2)
+      {
+        Throwing.Retract();
+        Gripper.Extend();
+        Serial.print("Piston_Press_Event:");
+        Serial.println(Piston_Press_Event);
+      }
+      break;
+
     case 'a':
-      if (prevCommand =='S') 
+      if (prevCommand == 'S')
       {
         Piston_Press_Event--;
-      } 
-        Piston_Press_Event %= 3;
-        if(Piston_Press_Event == 0)
-        {
-          Gripper.Extend();
-          Serial.print("Piston_Press_Event:");
-          Serial.println(Piston_Press_Event);
-        }
-        if(Piston_Press_Event == 1)
-        {
-          Throwing.Retract();
-          Serial.print("Piston_Press_Event:");
-          Serial.println(Piston_Press_Event);
-        }
-        if(Piston_Press_Event == 2)
-        {
-          Throwing.Extend();
-          Gripper.Retract(); 
-          Serial.print("Piston_Press_Event:");
-          Serial.println(Piston_Press_Event);  
-        }
+      }
+      Piston_Press_Event %= 3;
+      if (Piston_Press_Event == 0)
+      {
+        Gripper.Extend();
+        Serial.print("Piston_Press_Event:");
+        Serial.println(Piston_Press_Event);
+      }
+      if (Piston_Press_Event == 1)
+      {
+        Throwing.Retract();
+        Serial.print("Piston_Press_Event:");
+        Serial.println(Piston_Press_Event);
+      }
+      if (Piston_Press_Event == 2)
+      {
+        Throwing.Extend();
+        Gripper.Retract();
+        Serial.print("Piston_Press_Event:");
+        Serial.println(Piston_Press_Event);
+      }
       break;
 
     case 'b':
@@ -247,10 +247,10 @@ void loop()
       M3.SetSpeed(0);
 
       Serial.println("In Default");
- 
+
   }
-  
+
   prevCommand = command;
-        
-   #endif   
+
+#endif
 }
